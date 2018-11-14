de19a15aba243eceb686883245f3b99559f2f780
David Hill <david.hill@bison.howard.edu>
2018-11-14 17:07:57 -0500

    Going through React course

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Dave', age: 20},
+      {name: 'Baron', age: 17}
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log("Was clicked!")
+    //DONT DO this.state.persons[0].name = "David"
+    this.setState({
+      persons: [
+      {name: 'David', age: 20},
+      {name: 'Baron', age: 17}
+      ]
+    })
+  }
+
   render() {
-    return (
+
+    return (                                    //This is JSX not HTML
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Daves App Here!</h1>
+        <p>This is a paragraph</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobbies: Coding</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Gardening</Person>
+        <Person name="Mom" age="49"></Person>
       </div>
     );
+
+    //What the above gets compiled to.
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'DAVE!'))
   }
 }
 
